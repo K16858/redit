aa7e8b5ec101e5b04a5d8e46520027aebc672a88
k16858 <ktohda0127@gmail.com>
2025-10-24 08:54:42 +0900

    コード整理

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,6 @@
 use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
-use crossterm::execute;
-use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
-use std::io::stdout;
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor {
     should_quit: bool,
@@ -13,25 +12,10 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
-    }
-
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()?;
-        Self::draw_rows()
-    }
-
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
     }
 
     fn evaluate_event(&mut self, event: &Event) {
@@ -50,22 +34,23 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             println!("bye. \r\n");
+        } else {
+            Self::draw_rows()?;
         }
         Ok(())
     }
 
     fn draw_rows() -> Result<(), std::io::Error> {
-        let ternimal_size = crossterm::terminal::size()?;
+        let ternimal_size = Terminal::size()?;
         let rows_size = ternimal_size.1;
         let mut counter = rows_size;
         while counter > 0 {
             print!("~\r\n");
             counter -= 1;
         }
-        let mut stdout = stdout();
-        execute!(stdout, crossterm::cursor::MoveTo(1, 0))?;
+        Terminal::move_cursor_to(0, 0)?;
         Ok(())
     }
 
