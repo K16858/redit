8b6e033ae7f8a455456507d5760751f7e6678e50
k16858 <ktohda0127@gmail.com>
2025-10-27 00:06:45 +0900

    カーソル制御を用意

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -24,14 +24,10 @@ impl View {
     fn render_welcome_screen() -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
-            Terminal::clear_line()?;
             if current_row == height / 3 {
-                Self::draw_welcome_message()?;
+                Self::draw_welcome_message(current_row)?;
             } else {
-                Self::draw_empty_row()?;
-            }
-            if current_row + 1 < height {
-                Terminal::print("\r\n")?;
+                Self::render_line(current_row, "~")?;
             }
         }
         Ok(())
@@ -45,7 +41,7 @@ impl View {
         Ok(())
     }
 
-    fn draw_welcome_message() -> Result<(), Error> {
+    fn draw_welcome_message(at: usize) -> Result<(), Error> {
         let mut welcome_message = format!("{NAME} -- version {VERSION}");
         let width = Terminal::size()?.width;
         let len = welcome_message.len();
@@ -53,14 +49,14 @@ impl View {
         let spaces = " ".repeat(padding - 1);
         welcome_message = format!("~{spaces}{welcome_message}");
         welcome_message.truncate(width);
-        Terminal::print(&welcome_message)?;
+        Self::render_line(at, &welcome_message)?;
         Ok(())
     }
 
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-    }
+    // fn draw_empty_row() -> Result<(), Error> {
+    //     Terminal::print("~")?;
+    //     Ok(())
+    // }
 
     fn render_line(at: usize, line_text: &str) -> Result<(), Error> {
         Terminal::move_caret_to(Position { row: at, col: 0 })?;
