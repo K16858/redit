5c06c5e2f1f9be5eca68256b8b75d9c0f389893a
k16858 <ktohda0127@gmail.com>
2025-10-26 23:11:09 +0900

    ファイル読み込み関数を追加

--- src/editor/view/buffer.rs
+++ src/editor/view/buffer.rs
@@ -1,3 +1,6 @@
+use std::fs::read_to_string;
+use std::io::Error;
+
 pub struct Buffer {
     pub lines: Vec<String>,
 }
@@ -13,5 +16,14 @@ impl Default for Buffer {
 impl Buffer {
     pub fn is_empty(&self) -> bool {
         self.lines.is_empty()
+    }
+
+    pub fn load(file_name: &str) -> Result<Self, Error> {
+        let contents = read_to_string(file_name)?;
+        let mut lines = Vec::new();
+        for value in contents.lines() {
+            lines.push(String::from(value));
+        }
+        Ok(Self { lines })
     }
 }
