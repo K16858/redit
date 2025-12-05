e013a31a8fbcbe1c0381a79ae386ac7d6c5a87b9
k16858 <ktohda0127@gmail.com>
2025-12-05 09:36:23 +0900

    ミス修正

--- src/editor/view/line.rs
+++ src/editor/view/line.rs
@@ -137,7 +137,7 @@ impl Line {
 
     fn delete(&mut self, grapheme_index: usize) {
         let mut result = String::new();
-        for (index, fragment) in self.fragments.iter.enumerate() {
+        for (index, fragment) in self.fragments.iter().enumerate() {
             if index != grapheme_index {
                 result.push_str(&fragment.grapheme);
             }
